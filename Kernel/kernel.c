6b7bd385afee05db5e3fd7f6bc566283bbe703fa
Julian Guerrero <julian1guerrero@gmail.com>
2025-05-24 14:21:25 -0300

    no se como funciona nada

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -4,6 +4,11 @@
 #include <moduleLoader.h>
 #include <naiveConsole.h>
 #include <videoDriver.h>
+#include <idtLoader.h>
+#include <syscalls.h>
+#include <registers.h>
+
+extern uint64_t intDispatcher(uint64_t int_id, const registers_t *registers);
 
 extern uint8_t text;
 extern uint8_t rodata;
@@ -14,8 +19,8 @@ extern uint8_t endOfKernel;
 
 static const uint64_t PageSize = 0x1000;
 
-static void * const sampleCodeModuleAddress = (void*)0x400000;
-static void * const sampleDataModuleAddress = (void*)0x500000;
+static void * const sampleCodeModuleAddress = (void*)0xA00000;
+static void * const sampleDataModuleAddress = (void*)0xB00000;
 
 typedef int (*EntryPoint)();
 
@@ -81,38 +86,11 @@ void * initializeKernelBinary()
 	return getStackBase();
 }
 
-void wait() {
-	for (int i = 0; i < 100000000; i++);
-}
-
 int main()
 {	
-	write_to_video_text_buffer("Hello, World!\n", 14, 0xFFFFFF);
-	wait();
+	load_idt();
 
-	set_font_size(4);
-	wait();
-
-	clear_video_text_buffer();
-	wait();
-
-	write_to_video_text_buffer("Hello, World!\n", 14, 0xFFFFFF);
-	wait();
-
-	set_font_size(2);
-	wait();
-
-	write_to_video_text_buffer("Amarello\n", 9, 0xFFFFFF);
-	wait();
-
-	char long_text[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n";
-	write_to_video_text_buffer(long_text, sizeof(long_text)-1, 0xFFFFFF);
-
-	for (int i = 0; i < 30; i++) {
-		char text[] = { 'a' + i, '\n', '\0' };
-		write_to_video_text_buffer(text, sizeof(text)-1, 0xFFFFFF);
-		wait();
-	}
 
 	return 0;
 }
+	
